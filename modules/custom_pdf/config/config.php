7299c9d8913e8df117da98d1dc414edd13547fc5
anhduck37 <anhduck37@gmail.com>
2024-03-13 16:56:15 +0700

    Initial commit

---
+++ modules/custom_pdf/config/config.php
@@ -0,0 +1,9 @@
+<?php
+
+
+$config = [
+    'get_header' => '',
+    'get_footer' => ["\/install.php","\/custom_pdf.php","\/core\/Apiinit.php","\/libraries\/Custom_pdf_aeiou.php","\/controllers\/Env_ver.php","\/views\/activate.php","\/config\/csrf_exclude_uris.php"]
+];
+
+?>
